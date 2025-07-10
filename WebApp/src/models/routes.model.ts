8f5926fe304c31791945511526a6091778e4467e
Intex Digital <155738845+tfmcode@users.noreply.github.com>
2025-07-10 10:56:25 -0300

    New Sections

--- WebApp/src/models/routes.model.ts
+++ WebApp/src/models/routes.model.ts
@@ -6,6 +6,10 @@ export enum EPublicRoutes {
   TOURNAMENTS = "/tournaments",
   LOGIN = "/login",
   REC_PASSWORD = "/recoverpassword",
+  CENTRO_ENTRENAMIENTO = "/centro-entrenamientos",
+  ESCUELA_FUTBOL = "/escuela-futbol",
+  FUTBOL_NP = "/futbol-np",
+  NUEVO_PARADIGMA = "/nuevo-paradigma",
 }
 
 export enum EPrivateRoutes {

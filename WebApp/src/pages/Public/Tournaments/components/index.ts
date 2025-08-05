6011e6ef6062470aa90f2f157788fce65dd0aeb2
Intex Digital <155738845+tfmcode@users.noreply.github.com>
2025-08-05 11:58:35 -0300

    fix carpetas p2

--- WebApp/src/pages/Public/Tournaments/components/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as ModalFichaPartido } from './ModalFichaPartido';
-export { default as Sanctions } from './Sanctions';
-export { default as TableCards } from './TableCards';
-export { default as TableMatches } from './TableMatches';
-export { default as TablePosition } from './TablePosition';
-export { default as TableScorers } from './TableScorers';

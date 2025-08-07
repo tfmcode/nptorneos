f12c1076d733a8686738cc4a03d582f1170cc2dd
Intex Digital <155738845+tfmcode@users.noreply.github.com>
2025-08-07 10:41:36 -0300

    prd

--- WebApp/vite.config.ts
+++ WebApp/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './', 
   server: {
     port: Number(process.env.PORT) || 3000,
     host: '0.0.0.0' // 👈 Esto es CLAVE para que Railway acceda

1fb3cac74b3855c4a847cb3e5774be9b852abe58
Intex Digital <155738845+tfmcode@users.noreply.github.com>
2026-02-06 14:32:41 -0300

    fix seleccionar

--- WebApp/src/components/forms/DynamicForm.tsx
+++ WebApp/src/components/forms/DynamicForm.tsx
@@ -86,7 +86,7 @@ const DynamicForm: React.FC<DynamicFormProps> = ({
                 className="w-full px-3 py-2 border border-gray-300 rounded-md text-sm text-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 transition duration-200 disabled:opacity-50 disabled:cursor-not-allowed"
                 disabled={disabled}
               >
-                <option value="" disabled>
+                <option value="0" disabled>
                   Seleccionar...
                 </option>
                 {field.options.map((option, i) => (
